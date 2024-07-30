1d368370f4634067d56ec1f210cab97a2691743d
Vera Khatskevich <vkhatsk@icloud.com>
2024-07-30 20:27:36 +0000

    add popups

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
+const locations = [
+    {
+        name: "Bratislava",
+        href: 'locations/bratislava.html',
+        latLng: [48.148598, 17.107748],
+    },
+    {
+        name: "Viena",
+        href: 'locations/vienna.html', latLng: [48.210033, 16.363449]
+    },
+];
+
+const map = L.map('map').setView(locations[0].latLng, 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-const marker = L.marker([51.5, -0.09]).addTo(map);
+locations.forEach((location) => {
+    L.marker(location.latLng).addTo(map).bindPopup(`<a href=${location.href}>${location.name}</a>`);
+})
